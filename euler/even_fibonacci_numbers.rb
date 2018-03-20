ba7f321f6ae089c501a78bc267269abd897309cd
Alen Miletic <alenmiletic@tentamen.local>
2018-03-20 10:03:30 +0100

    Added improved fibonacci sequence code for finding a sum of the even numbers below 4000000

--- euler/even_fibonacci_numbers.rb
+++ euler/even_fibonacci_numbers.rb
@@ -1,3 +1,5 @@
+=begin 
+#first code
 sum_of_even_valued_numbers = 0
 array = Array.new
 array [0] = 1
@@ -12,3 +14,23 @@ end
 array.each{|number| sum_of_even_valued_numbers += number if number < 4000000 && number % 2 == 0}
 puts sum_of_even_valued_numbers
 puts array
+
+=end
+
+#second(improved) code
+#This code adds every third number in the sequence because every third number is an even number
+
+limit = 4000000
+sum = 0
+a = 1
+b = 1
+c = a + b
+
+while c < limit do
+    sum = sum + c
+    a = b + c
+    b = c + a
+    c = a + b
+end
+
+puts sum
